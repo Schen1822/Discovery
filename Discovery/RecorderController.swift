b6e754aeb5221730e01767aa01a3e2e69d757dfe
Steven Chen <schen1822@berkeley.edu>
2021-04-19 12:32:33 -0700

    files

--- Discovery/RecorderController.swift
+++ Discovery/RecorderController.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import AVFoundation
+import Accelerate
+
+enum RecorderState {
+    case recording
+    case stopped
+    case denied
+}
+
+protocol RecorderViewControllerDelegate: class {
+    func didStartRecording()
+    func didFinishRecording()
+}
 
 class RecorderController: UIViewController {
     var recordButton = RecordButton()
     var handleView = UIView()
     var timeLabel = UILabel()
     var prompt = UILabel()
-    //var audioView = AudioVisualizerView()
-    
+    var audioView = AudioVisualizerView()
+    let audioEngine = AVAudioEngine()
+    weak var delegate: RecorderViewControllerDelegate?
+    private var recordingTs: Double = 0
+    private var silenceTs: Double = 0
+    private var renderTs: Double = 0
+    private var audioFile: AVAudioFile?
+    let settings = [AVFormatIDKey: kAudioFormatLinearPCM, AVLinearPCMBitDepthKey: 16, AVLinearPCMIsFloatKey: true, AVSampleRateKey: Float64(44100), AVNumberOfChannelsKey: 1] as [String : Any]
     
     @objc func handleRecording(_ sender: RecordButton) {
             var defaultFrame: CGRect = CGRect(x: 0, y: 24, width: view.frame.width, height: 135)
             if recordButton.isRecording {
                 defaultFrame = self.view.frame
-                //audioView.isHidden = false
+                audioView.isHidden = false
                 UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                     //self.handleView.alpha = 1
-                    //self.timeLabel.alpha = 1
-                    //self.audioView.alpha = 1
+                    self.timeLabel.alpha = 1
+                    self.audioView.alpha = 1
                     //self.view.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.bounds.width, height: -300)
                     //self.view.layoutIfNeeded()
                 }, completion: nil)
@@ -38,6 +57,173 @@ class RecorderController: UIViewController {
                     //self.view.layoutIfNeeded()
                 }, completion: nil)
                 //self.stopRecording()
+            }
+        }
+    
+    private func format() -> AVAudioFormat? {
+            let format = AVAudioFormat(settings: self.settings)
+            return format
+        }
+    private func createAudioRecordPath() -> URL? {
+            let format = DateFormatter()
+            format.dateFormat="yyyy-MM-dd-HH-mm-ss-SSS"
+            let currentFileName = "recording-\(format.string(from: Date()))" + ".wav"
+            let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let url = documentsDirectory.appendingPathComponent(currentFileName)
+            return url
+        }
+    private func createAudioRecordFile() -> AVAudioFile? {
+            guard let path = self.createAudioRecordPath() else {
+                return nil
+            }
+            do {
+                let file = try AVAudioFile(forWriting: path, settings: self.settings, commonFormat: .pcmFormatFloat32, interleaved: true)
+                return file
+            } catch let error as NSError {
+                print(error.localizedDescription)
+                return nil
+            }
+        }
+    private func startRecording() {
+            if let d = self.delegate {
+                d.didStartRecording()
+            }
+            
+            self.recordingTs = NSDate().timeIntervalSince1970
+            self.silenceTs = 0
+            
+            do {
+                let session = AVAudioSession.sharedInstance()
+                try session.setCategory(.playAndRecord, mode: .default)
+                try session.setActive(true)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+                return
+            }
+            
+            let inputNode = self.audioEngine.inputNode
+            guard let format = self.format() else {
+                return
+            }
+            
+            inputNode.installTap(onBus: 0, bufferSize: 1024, format: format) { (buffer, time) in
+                let level: Float = -50
+                let length: UInt32 = 1024
+                buffer.frameLength = length
+                let channels = UnsafeBufferPointer(start: buffer.floatChannelData, count: Int(buffer.format.channelCount))
+                var value: Float = 0
+                vDSP_meamgv(channels[0], 1, &value, vDSP_Length(length))
+                var average: Float = ((value == 0) ? -100 : 20.0 * log10f(value))
+                if average > 0 {
+                    average = 0
+                } else if average < -100 {
+                    average = -100
+                }
+                let silent = average < level
+                let ts = NSDate().timeIntervalSince1970
+                if ts - self.renderTs > 0.1 {
+                    let floats = UnsafeBufferPointer(start: channels[0], count: Int(buffer.frameLength))
+                    let frame = floats.map({ (f) -> Int in
+                        return Int(f * Float(Int16.max))
+                    })
+                    DispatchQueue.main.async {
+                        let seconds = (ts - self.recordingTs)
+                        self.timeLabel.text = seconds.toTimeString
+                        self.renderTs = ts
+                        let len = self.audioView.waveforms.count
+                        for i in 0 ..< len {
+                            let idx = ((frame.count - 1) * i) / len
+                            let f: Float = sqrt(1.5 * abs(Float(frame[idx])) / Float(Int16.max))
+                            self.audioView.waveforms[i] = min(49, Int(f * 50))
+                        }
+                        self.audioView.active = !silent
+                        self.audioView.setNeedsDisplay()
+                    }
+                }
+                
+                let write = true
+                if write {
+                    if self.audioFile == nil {
+                        self.audioFile = self.createAudioRecordFile()
+                    }
+                    if let f = self.audioFile {
+                        do {
+                            try f.write(from: buffer)
+                        } catch let error as NSError {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+            do {
+                self.audioEngine.prepare()
+                try self.audioEngine.start()
+            } catch let error as NSError {
+                print(error.localizedDescription)
+                return
+            }
+            self.updateUI(.recording)
+        }
+        
+        private func stopRecording() {
+            if let d = self.delegate {
+                d.didFinishRecording()
+            }
+            
+            self.audioFile = nil
+            self.audioEngine.inputNode.removeTap(onBus: 0)
+            self.audioEngine.stop()
+            do {
+                try AVAudioSession.sharedInstance().setActive(false)
+            } catch  let error as NSError {
+                print(error.localizedDescription)
+                return
+            }
+            self.updateUI(.stopped)
+        }
+    
+    private func updateUI(_ recorderState: RecorderState) {
+            switch recorderState {
+            case .recording:
+                UIApplication.shared.isIdleTimerDisabled = true
+                self.audioView.isHidden = false
+                self.timeLabel.isHidden = false
+                break
+            case .stopped:
+                UIApplication.shared.isIdleTimerDisabled = false
+                self.audioView.isHidden = true
+                self.timeLabel.isHidden = true
+                break
+            case .denied:
+                UIApplication.shared.isIdleTimerDisabled = false
+                self.recordButton.isHidden = true
+                self.audioView.isHidden = true
+                self.timeLabel.isHidden = true
+                break
+            }
+        }
+    
+    private func checkPermissionAndRecord() {
+            let permission = AVAudioSession.sharedInstance().recordPermission
+            switch permission {
+            case .undetermined:
+                AVAudioSession.sharedInstance().requestRecordPermission({ (result) in
+                    DispatchQueue.main.async {
+                        if result {
+                            self.startRecording()
+                        }
+                        else {
+                            self.updateUI(.denied)
+                        }
+                    }
+                })
+                break
+            case .granted:
+                self.startRecording()
+                break
+            case .denied:
+                self.updateUI(.denied)
+                break
             }
         }
     
